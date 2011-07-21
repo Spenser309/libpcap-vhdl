ce8678f4bf271698d9785de486aa02023c5d7845
Spenser Gilliland <Spenser309@gmail.com>
2011-07-21 05:13:40 -0500

    Worked a bit on this today

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <pcap/pcap.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 int timeval_subtract(struct timeval*, struct timeval*, struct timeval*);
 
 int main(int argc, char *argv[])
 {
+   int i;
 	char *input_file = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
 
@@ -29,16 +31,23 @@ int main(int argc, char *argv[])
 	
 	printf("Opened %s attempting to read packet lengths\n", input_file);
 
-	while(pcap_next_ex(packets, &header, &packet) != -2)
+   pcap_next_ex(packets, &header, &packet);
+   ts_old = header->ts;
+
+   do
 	{
 		if(timeval_subtract(&elapsed_time, &(header->ts), &ts_old) == 0)
 		{
-			printf("Packet of length [%d] arrived %li s and %li us later",header->len,  (long int) elapsed_time.tv_sec, (long int) elapsed_time.tv_usec);
-			
+			printf("Packet of length [%d] arrived %li s and %li us later\n",header->len,  (long int) elapsed_time.tv_sec, (long int) elapsed_time.tv_usec);
+         for(i = 0; i < header->len; i++) {
+            if (i % 8 == 0 && i != 0) printf("\n");
+               printf("%2X ", packet[i]);
+         }
+         printf("\n");
 		}
 		ts_old = header->ts;
 		getchar();
-	}
+   } while(pcap_next_ex(packets, &header, &packet) != -2); 
 
 	printf("Thats all she wrote folks\n");
 		
